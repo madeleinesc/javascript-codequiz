1b2772d9f3efc2b0cf4d6af6810c1d695e941861
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-09-27 18:30:03 +0930

    wrote up quiz questions in comments

---
+++ assets/js/quiz.js
@@ -0,0 +1,40 @@
+// QUIZ GAME PAGE
+
+// Rules:
+// Test your knowledge of Javascript with this timed quiz!
+// This is a multiple choice quiz with code-related questions.
+// You must pick an answer within the time limit.
+// Any incorrect answers will penalize your score time by 10 seconds.
+// Press Play to start the quiz now!
+
+
+// QUESTIONS:
+// Which of the following is not a Javascript data type? 
+// 1. undefined, 2. number, 3. boolean, 4. float.    correct answer: 4. float.
+
+// Javascript ignores...
+// 1. spaces, 2. tabs, 3. newlines, 4.all of the above.   correct answer: 4
+
+// Which of the following in not a proper looping structure in Javascript?
+// 1. for , 2. forwhich , 3. while , 4. dowhile       correct answer: 3
+
+// Javascript is ideal to..
+// 1. make computations in HTML simpler , 2. minimise storage requirements on the web server , 3. increase the download time for the client , 4. none of the above         correct answer: 2
+
+// Which syntax is used to get a particular value using the tagged name?
+// 1. getElementID() , 2. getElementsbyName() , 3. getElementsbyTageName() , 4. getTagName()         correct answer: 3
+
+// What are variables used for in Javascript?
+// 1. storing numbers, dates or other values , 2. varying randomly , 3. causing high-school algebra flashbacks , 4. none of the above    correct answer: 1
+
+// Which of the following is not a valid Javascript variable name?
+// 1. 2names , 2. _first_and_last_names , 3. FirstAndLast , 4. none of the above          correct: 1
+
+// Using the ... statement is how you test for a specific condition.
+// 1. select , 2. if , 3. switch , 4. for       correct answer: 2
+
+// Inside which HTML element do we put the Javascript?
+// 1. <javascript> , 2. <js> , 3. <script> , 4. <scripting>   correct answer: 3
+
+// How do you write 'Hello World' in an alert box?
+// 1. msgBox('Hello World'); , 2. alertBox('Hello World'); , 3. msg('Hello World'); , 4. alert('Hello World');   correct answer: 4
